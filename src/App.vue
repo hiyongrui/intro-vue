26b707422fccbe3cbdca73433ec4c54aae2974df
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-11-29 18:54:21 +0800

    lifecycle hook beforeCreate(), created()
    https://vuejs.org/guide/essentials/lifecycle.html
    
    for Options API, should run the fetch code in created() and not beforeCreate()

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,17 @@ export default {
   methods: {
     async fetchPokemon() {
       this.pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => res.json());
-
-    },
+    }
+  },
+  // lifecycle hooks
+  beforeCreate() {
+    console.log("beforeCreate");
+    console.log(this.pokemon);
+  },
+  created() {
+    console.log("created");
+    console.log(this.pokemon);
+    // this.fetchPokemon();
   }
 }
 </script>
