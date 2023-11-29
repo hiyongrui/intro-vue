99df8697f9090e6b6ad08f15e6d68fb4079ac684
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-11-29 18:49:20 +0800

    fetch data of pokemon example
    fetch is async operation, so need to wait for fetch to finish, use async await or .then()
    
    <pre> tag to show json format
    
    move previous code to scratchpad

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,22 @@
 <script>
-import BenderStatistics from "./components/BenderStatistics.vue";
-import UserCard from "./components/UserCard.vue";
-import CharacterCard from "./components/CharacterCard.vue";
-import BaseButton from "./components/BaseButton.vue";
-import BaseLayout from "./components/BaseLayout.vue";
-
+// https://pokeapi.co/api/v2/pokemon?limit=151
 export default {
-  components: {
-    BenderStatistics,
-    UserCard,
-    CharacterCard,
-    BaseButton,
-    BaseLayout,
-  },
-  data: () => ({
-    newCharacter: {
-      name: "",
-      element: [],
-    },
-    characterList: [
-      {
-        name: "Aang",
-        element: ["Air", "Earth", "Water", "Fire"],
-      },
-      {
-        name: "Zuko",
-        element: ["Fire"],
-      },
-      {
-        name: "Toph",
-        element: ["Earth"],
-      },
-      {
-        name: "Katara",
-        element: ["Water"],
-      },
-    ],
-    favoriteList: [],
-    userData: {
-      name: "ben",
-      favoriteFood: "pizza",
-    },
-  }),
-  computed: {
-    refinedUserData() {
-      return {
-        name: this.userData.name.toUpperCase(),
-        food: this.userData.favoriteFood.toUpperCase(),
-      };
-    },
+  data() {
+    return {
+      pokemon: [1, 2, 3],
+    };
   },
   methods: {
-    addNewCharacter() {
-      this.characterList.push(this.newCharacter);
-      this.newCharacter = { name: "" };
+    async fetchPokemon() {
+      this.pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => res.json());
+
     },
-    addFavoriteCharacter(payload) {
-      this.favoriteList.push(payload);
-    },
-    changeName() {
-      this.userData.name = "Charlie";
-    },
-  },
-};
+  }
+}
 </script>
 
 <template>
-  <BaseLayout>
-    <template v-slot:two>
-      <h2>New Character</h2>
-      <pre>{{ newCharacter }}</pre>
-      <label for="character-name">Name</label>
-      <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter" />
-      <p>
-        <span v-for="(character, index) in characterList" :key="`comma-list-character-${index}`">
-          {{ character.name }}{{ index === characterList.length - 1 ? "" : ", " }}
-        </span>
-      </p>
-    </template>
-  </BaseLayout>
-  <BaseButton :left="true"></BaseButton>
-
-  <UserCard :user="refinedUserData" @change-name="changeName" />
-  <BenderStatistics :characters="characterList" />
-  <h2>Characters</h2>
-  <p v-if="characterList.length === 0">There are no characters</p>
-  <ul v-else-if="characterList.length % 2 === 0">
-    <li v-for="(character, index) in characterList" :key="`even-character-${index}`">
-      <!--character card placeholder-->
-      <CharacterCard :character="character" @favorite="addFavoriteCharacter" />
-    </li>
-  </ul>
-  <p v-else>There are odd characters!</p>
-  <h2>Favorite Characters</h2>
-  <ul v-if="favoriteList.length > 0">
-    <li v-for="(character, index) in favoriteList" :key="`odd-character-${index}`">
-      {{ character }}
-    </li>
-  </ul>
-  <p v-else>No favorite characters yet!</p>
+  <h1>New app</h1>
+  <pre> {{ pokemon }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
 </template>
