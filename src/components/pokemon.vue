dada55e633851d82a1c5ef65003de10b53ac4974
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-11-30 15:43:19 +0800

    ref, computed, reactive
    ref are helper functions from vue to give reactivity to the variable, wrapped the value to be reactive
    
    computed compositionAPI is similar to the options API computed, but have to use .value() to unwrap it, this also signifies to people that when working in vanilla JavaScript, that this value is attached to Vue reactivity
    
    if don't like ref and don't want to use .value, use reactive() helper function, which allows customisation like instead of data() in OptionsAPI, can call it state()

--- src/components/pokemon.vue
+++ src/components/pokemon.vue
@@ -1,14 +1,39 @@
 <script>
 // https://pokeapi.co/api/v2/pokemon?limit=151
+import { ref, computed, reactive } from "vue";
+
 export default {
   async setup() {
-    const regionName = "kanto";
+    const regionName = ref("Kanto");
+    console.log(regionName);
+
+    const compositionAPI_Computed_RegionNameAllCaps = computed(() => regionName.value.toUpperCase());
+
+    //reactive feels like option API data() function, if don't want to call it data() call it state(), example below, using this also mean don't have to .value unlike above
+    const state = reactive({
+      elementType: "lightning",
+    });
+    console.log(state);
+    const compositionAPI_reactive_RegionNameAllCaps = computed(() => state.elementType.toUpperCase());
+
     const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151").then((res) => res.json());
 
     return {
       pokemon,
       regionName,
+      compositionAPI_Computed_RegionNameAllCaps,
+      compositionAPI_reactive_RegionNameAllCaps,
     };
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = "Hoenn";
+    },
+  },
+  computed: {
+    optionsAPI_Computed_RegionNameLowerCase() {
+      return this.regionName.toLowerCase();
+    },
   },
   // lifecycle hooks
   beforeCreate() {
@@ -25,5 +50,10 @@ export default {
 </script>
 
 <template>
+  <h2>{{ regionName }}</h2>
+  <h3>{{ compositionAPI_Computed_RegionNameAllCaps }}</h3>
+  <h3>{{ compositionAPI_reactive_RegionName }}</h3>
+  <h3>{{ optionsAPI_Computed_RegionNameLowerCase }}</h3>
+  <button @click="changeRegionName">Change Region Name</button>
   <pre> {{ pokemon }}</pre>
 </template>
