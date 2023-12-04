f92a9576114ff2fadfc2110eca055bd02a81f63b
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-12-04 15:55:39 +0800

    v-bind in css
    color picker to change color based on javascript using ref(), binding with v-model and v-bind in css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import BaseCounter from "./components/BaseCounter.vue";
 import UserCard from "./components/UserCard.vue";
+import { ref } from "vue";
+
+const colorPreference = ref("white");
 </script>
 
 <template>
-  <BaseCounter />
-  <UserCard :user="{ name: 'Ben', food: 'Cheeseburger' }" />
+  <div class="wrapper">
+    <h2>{{ colorPreference }}</h2>
+    <input type="color" v-model="colorPreference" />
+    <BaseCounter />
+    <UserCard :user="{ name: 'Ben', food: 'Cheeseburger' }" />
+  </div>
 </template>
 
 <style>
@@ -15,5 +22,9 @@ html {
 
 .button {
   border: 10px solid red;
+}
+
+.wrapper {
+  background-color: v-bind(colorPreference);
 }
 </style>
