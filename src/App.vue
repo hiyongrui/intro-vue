f4a140eb27b15619874198ae9e825ea317bb9323
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-12-06 11:15:33 +0800

    programmatic navigation router
    create router using useRouter() helper method from vue
    
    watch() when value > 1200, navigate to other page
    
    difference between router.push() vs router.replace()
    - https://stackoverflow.com/a/65933466 below
    I like to use .replace() when i have a definitive action and i dont want the user to be able to come back to this page.
    
    Example: i have a product detail page with a delete button to ultimately deletes the product. When i click the button i do router.replace back to the list of all products so when the user uses the back button in the browser he is not able to go back to the details page so he cannot see or even click again the delete button as this product does not exist anymore.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
-import BaseCounter from "./components/BaseCounter.vue";
-import UserCard from "./components/UserCard.vue";
-import { ref } from "vue";
+import BaseCounter from "@/components/BaseCounter.vue";
+import UserCard from "@/components/UserCard.vue";
+import { ref, watch } from "vue";
+import { useCount } from "@/composables/countStore";
+import { useRouter } from "vue-router";
 
 const colorPreference = ref("white");
+
+const countStore = useCount();
+
+const router = useRouter();
+
+watch(countStore.globalCount, (val) => {
+  if (val > 1200) {
+    // navigate user to pokemon page
+    router.push("/pokemon");
+  }
+});
 </script>
 
 <template>
@@ -12,6 +25,7 @@ const colorPreference = ref("white");
       <router-link to="/">Home</router-link>
       <router-link to="/pokemon">Pokemon</router-link>
     </nav>
+    <h2>{{ countStore.globalCount }}</h2>
     <router-view />
     <h2>{{ colorPreference }}</h2>
     <input type="color" v-model="colorPreference" />
